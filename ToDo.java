deb98b7c0be5c5ebc1c921d1b32034c0cf34d4fa
Max Danigel <maxdanigel@MacBook-Pro-von-Max.fritz.box>
2026-02-10 23:02:52 +0100

    Methoden aus Knoten in ToDo kopiert.

--- ToDo.java
+++ ToDo.java
@@ -11,4 +11,54 @@ public class ToDo {
 	private LocalDate date;
 	private LocalTime time;
 	private int counter = 0;
+
+	public ToDo(String value, int priority){ // Default Konstruktor, der jedes mal mit einem Scanner die Eingabe überprüft
+		setValue(value);
+		setPriority(priority);
+		time = LocalTime.now().withSecond(0).withNano(0); // Setzt Sekunden und Nano direkt auf 0, damit sie nicht mit geprintet werden.
+		this.date = LocalDate.now();
+		//TODO: Datum und Uhrzeit noch weiter anpassen, bis es schön aussieht.
+	}
+	/*
+	@Setter Legt den Wert für die To-Do fest.
+	@Exception Überprüft ob etwas in das To-Do Feld eingegeben wurde, oder ob es mit null belegt wurde.
+	 */
+	public void setValue(String value)throws NullPointerException, IllegalArgumentException{
+
+		if(value == null){
+			throw new NullPointerException("Du kannst keine To-Do mit null belegen!");
+		}
+		if(value.equals("")){
+			throw new IllegalArgumentException("Du hast vergessen etwas einzutragen!");
+		}
+		this.value = value;
+	}
+
+
+	/*
+	@Setter Legt die Priorität der To-Do fest.
+	@Exception Überprüft ob der Wert zwischen 1-3 liegt.
+	 */
+	public void setPriority(int priority)throws IllegalArgumentException{
+		if(priority <= 0 || priority > 3){
+			throw new IllegalArgumentException("Der Wert darf nicht unter 1 oder über 3 sein!");
+		}
+		this.priority = priority;
+	}
+	public void setCounter(int counter){
+		this.counter = counter;
+	}
+	public int getPriority(){
+		return this.priority;
+	}
+	public String getValue(){
+		return value;
+	}
+	/*
+	@ToString Wie das Objekt auf dem Terminal geprintet wird.
+	 */
+	public String toString(){
+		return "" + counter + ": " + this.value + " (prio:" + this.priority + ") | Erstellt am: " + this.date + " | Uhrzeit: " + this.time;
+	}
+
 }
