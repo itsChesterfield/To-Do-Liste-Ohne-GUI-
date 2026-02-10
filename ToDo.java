6c4fb5a9e673e8153b9f47fc145695e529d9ef0b
Max Danigel <maxdanigel@MacBook-Pro-von-Max.fritz.box>
2026-02-10 22:57:43 +0100

    Kommentar hinzugefügt.

--- ToDo.java
+++ ToDo.java
@@ -1,6 +1,10 @@
 package liste;
 import java.time.LocalDate;
 import java.time.LocalTime;
+/*
+Um alles Seperat zu erhalten, habe ich eine extra ToDo Klasse erstellt, für das do one Prinzip!
+ */
 
 public class ToDo {
+
 }
